9bec6242d8d406fdd2b8218a407115bb625e549f
CAdev1234 <fenglong37@outlook.com>
2022-09-12 04:55:01 -0400

    deploy update url

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import {PageFinal, PageHome} from './pages';
 import {Header, Layout1} from '@components/layouts';
 
@@ -9,8 +9,15 @@ const App: React.FC = () => (
         <Header />
         <Layout1>
             <Routes>
-                <Route path="/" element={<PageHome />} />
-                <Route path="/final" element={<PageFinal />} />
+                <Route path="/distance-calculator/" element={<PageHome />} />
+                <Route
+                    path="/distance-calculator/final"
+                    element={<PageFinal />}
+                />
+                <Route
+                    path="*"
+                    element={<Navigate to="/distance-calculator/" replace />}
+                />
             </Routes>
         </Layout1>
     </>
